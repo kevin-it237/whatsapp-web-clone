18e73affabf6a15d00b7cf07d197674f74aa1a5b
kevin-it237 <ngaleuabel@gmail.com>
2020-08-08 19:04:02 +0100

    Updates

---
+++ precache-manifest.2f0658f2691393502182f1e7611d099b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fea73e2f73d34291a8cd49e7c37476",
+    "url": "/whatsapp-web-clone/index.html"
+  },
+  {
+    "revision": "2634342eb24e29e8b3d4",
+    "url": "/whatsapp-web-clone/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "847b889f52a8fb4decca",
+    "url": "/whatsapp-web-clone/static/css/main.ecaa060c.chunk.css"
+  },
+  {
+    "revision": "2634342eb24e29e8b3d4",
+    "url": "/whatsapp-web-clone/static/js/2.a76b3a22.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/whatsapp-web-clone/static/js/2.a76b3a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847b889f52a8fb4decca",
+    "url": "/whatsapp-web-clone/static/js/main.5c80d2c5.chunk.js"
+  },
+  {
+    "revision": "6221a2e12c6c2334785a",
+    "url": "/whatsapp-web-clone/static/js/runtime-main.56e86bc3.js"
+  },
+  {
+    "revision": "146b579639d2399c148c9e5a3ac74ed3",
+    "url": "/whatsapp-web-clone/static/media/1.146b5796.jpg"
+  },
+  {
+    "revision": "a09bdb4a3492ad365d812a71dc296635",
+    "url": "/whatsapp-web-clone/static/media/10.a09bdb4a.jpg"
+  },
+  {
+    "revision": "df1ddcec7fce65dc5e8de1cb6506aca8",
+    "url": "/whatsapp-web-clone/static/media/2.df1ddcec.jpg"
+  },
+  {
+    "revision": "d911143595e1644cb0c373f46b1421d6",
+    "url": "/whatsapp-web-clone/static/media/3.d9111435.jpeg"
+  },
+  {
+    "revision": "78ac5fc19377c1faaa756f95b1080139",
+    "url": "/whatsapp-web-clone/static/media/4.78ac5fc1.jpg"
+  },
+  {
+    "revision": "a2101adca7f05c37eeb4414ce3248992",
+    "url": "/whatsapp-web-clone/static/media/5.a2101adc.jpg"
+  },
+  {
+    "revision": "66215a9aeaff45ad7ae10aa06923832a",
+    "url": "/whatsapp-web-clone/static/media/6.66215a9a.png"
+  },
+  {
+    "revision": "a770ff2a690554836375e6270d192573",
+    "url": "/whatsapp-web-clone/static/media/7.a770ff2a.jpg"
+  },
+  {
+    "revision": "ea2b2a373368f46149baf2655461f2f7",
+    "url": "/whatsapp-web-clone/static/media/8.ea2b2a37.jpg"
+  },
+  {
+    "revision": "9e8a2898faedb7db9bf5638405cf81ae",
+    "url": "/whatsapp-web-clone/static/media/bg-chat.9e8a2898.png"
+  },
+  {
+    "revision": "e340ee4d698cd93e37a32118c593faf6",
+    "url": "/whatsapp-web-clone/static/media/img1.e340ee4d.jpg"
+  },
+  {
+    "revision": "cc5148ad636baa9fae18a89913314124",
+    "url": "/whatsapp-web-clone/static/media/img2.cc5148ad.jpg"
+  },
+  {
+    "revision": "e17140be5003d16ffcf6ffef1eb8bfc0",
+    "url": "/whatsapp-web-clone/static/media/img3.e17140be.jpg"
+  },
+  {
+    "revision": "ee0cdb86f7074e357a7944252ddb97f3",
+    "url": "/whatsapp-web-clone/static/media/img4.ee0cdb86.jpg"
+  },
+  {
+    "revision": "8ea19d20e484aa57218dadac9019c583",
+    "url": "/whatsapp-web-clone/static/media/img5.8ea19d20.jpg"
+  },
+  {
+    "revision": "dcb0a74420cf64f216c786e34120ca3a",
+    "url": "/whatsapp-web-clone/static/media/me.dcb0a744.jpg"
+  }
+]);
